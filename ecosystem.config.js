7fb956f257e4e54342f74b35be4a183cc7e84bec
dokmironov@gmail.com <Edward!1488@>
2025-07-23 17:51:38 +0600

    add ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: "hahazen",
+            script: "npm",
+            args: "start",
+            env: {
+                PORT: 3001
+            }
+        }
+    ]
+};
